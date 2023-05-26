a530969d3a92f9144de5c53f2a8816d8cc7ae150
Vitor <vitor.daniel@outlook.pt>
2023-05-26 12:25:26 +0100

    fix Program errors

--- Game/Card.cs
+++ Game/Card.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LP1Projeto2
+namespace Game
 {
     /// <summary>
     /// class with list of card names and attributes to use in game
@@ -55,23 +55,5 @@ namespace LP1Projeto2
             set { dp = value; }
         }
     }
-    //create list for card types
-    List<Card> Deck = new List<Card>();
-    //create each card type
-    Deck.Add(new Card("Flying Wind", 1, 1, 1));
-    Deck.Add(new Card("Severed Monkey Head", 1, 2, 1));
-    Deck.Add(new Card("Mystical Rock Head", 2, 0, 5));
-    Deck.Add(new Card("Lobster MCCrabs", 2, 1, 3));
-    Deck.Add(new Card("Goblin Troll", 3, 3, 2));
-    Deck.Add(new Card("Scorching Heatwave", 4, 5, 3));
-    Deck.Add(new Card("Blind Minotaurs", 3, 1, 3));
-    Deck.Add(new Card("Tim, The Wizard", 5, 6, 4));
-    Deck.Add(new Card("Sharply Dressed", 4, 3, 3));
-    Deck.Add(new Card("Blue Steel", 1, 1, 1));
 
-    foreach (var card in Deck)
-    {
-        Console.WriteLine("Card: {0},{1},{2},{3}", card.Name, card.cost, 
-                                                    card.ap, card.dp);
-    }
 }
--- Game/HealthPoints.cs
+++ Game/HealthPoints.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LP1Projeto2
+namespace Game
 {
     public class HealthPoints
     {
