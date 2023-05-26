8d04c26d1f7e5b220ec297ac4d7670d380bea5c3
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-05-26 10:53:37 +0100

    Card stats

--- Cards.cs
+++ Cards.cs
@@ -5,8 +5,21 @@ using System.Threading.Tasks;
 
 namespace LP1Projeto2
 {
+    /// <summary>
+    /// Stats for each card
+    /// </summary>
     public class Cards
     {
-        
+        //Name
+        public string Name { get; set; }
+        //MP (Mana Points) ccost
+        public int Cost { get; set; }
+        //AP (Attack Points)
+        public int AttackPoints { get; set; }
+        //DP (Defense Points)
+        public int DefensePoints { get; set; }
+        //How many of them there are
+        public int Quantity { get; set; }
     }
+
 }
