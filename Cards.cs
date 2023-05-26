6996fd70b7db2e19c4767203b790f4b34b863d9d
Vitor <vitor.daniel@outlook.pt>
2023-05-26 11:01:14 +0100

    update Cards class

--- Cards.cs
+++ Cards.cs
@@ -5,8 +5,20 @@ using System.Threading.Tasks;
 
 namespace LP1Projeto2
 {
-    public class Cards
+    /// <summary>
+    /// class with list of card names and attributes to use in game
+    /// </summary>
+    abstract class Cards
     {
-        
+        //Name
+        public string Name { get; set; }
+        //MP (Mana Points) ccost
+        public int Cost { get; set; }
+        //AP (Attack Points)
+        public int AttackPoints { get; set; }
+        //DP (Defense Points)
+        public int DefensePoints { get; set; }
+        //How many of them there are
+        public int Quantity { get; set; }
     }
 }
