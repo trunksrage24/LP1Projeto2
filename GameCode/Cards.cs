442a993834cbf47f1b17fc50caeb7763e9a7ca1a
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-06-03 12:37:18 +0100

    Added XML comments

--- GameCode/Cards.cs
+++ GameCode/Cards.cs
@@ -10,13 +10,25 @@ namespace GameCode
     /// </summary>
     public class Card
     {
+        //Name of the card
         private string name;
+        //How much mana it costs
         private int cost;
+        //Attack to reduce DP and health of player
         private int ap;
+        //Defense against AP
         private int dp;
+        //How many there are
         private int quantity;
 
-        // Constructor for card types
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="cost"></param>
+        /// <param name="ap"></param>
+        /// <param name="dp"></param>
+        /// <param name="quantity"></param>
         public Card(string name, int cost, int ap, int dp, int quantity)
         {
             this.name = name;
@@ -26,31 +38,35 @@ namespace GameCode
             this.quantity = quantity;
         }
 
-        // Getter and setter for each variable:
+        //Get and set the name
         public string Name
         {
             get { return name; }
             set { name = value; }
         }
 
+        //Get and set the cost
         public int Cost
         {
             get { return cost; }
             set { cost = value; }
         }
 
+        //Get and set the attack power
         public int AP
         {
             get { return ap; }
             set { ap = value; }
         }
 
+        //Get and set the defense power
         public int DP
         {
             get { return dp; }
             set { dp = value; }
         }
 
+        //Get and set the quantity
         public int Quantity
         {
             get { return quantity; }
--- GameCode/Players.cs
+++ GameCode/Players.cs
@@ -10,11 +10,11 @@ namespace GameCode
     /// </summary>
     public class Player
     {
-        //Health
+        //Health of player
         public int HealthPoints { get; set; }
-        //MP 
+        //MP or Mana that player can use per turn
         public int ManaPoints { get; set; }
-        //Maximum MP for the current turn
+        //Maximum MP for each turn
         public int MaxMana { get; set; }
         //List of 20 cards
         public List<Card> Deck { get; set; }
@@ -25,18 +25,30 @@ namespace GameCode
 
         public List<Card> CombatList { get; set; } = new List<Card>();
 
+        /// <summary>
+        /// Mana increment and check if turn is true
+        /// </summary>
         public void StartTurn()
         {
+            //If the turn is true then the game continues
             IsTurn = true;
-            MaxMana = Math.Min(5, MaxMana + 1); //Increase MaxMana by 1 up to a maximum of 5
+            //Max mana will increase +1 every turn until 5
+            MaxMana = Math.Min(5, MaxMana + 1); 
+            //Mana points utilized in game, will update every turn to be Max.
             ManaPoints = MaxMana;
         }
 
+        /// <summary>
+        /// If turn is false
+        /// </summary>
+        /// <param name="turnCount"></param>
         public void EndTurn(int turnCount)
         {
+            //If turn ends then allows for other processes.
             IsTurn = false;
+            //Allow processes to play
             Console.WriteLine($"Player {(IsTurn ? 1 : 2)} ends their turn.");
-            // Add any logic you need to perform at the end of the turn
+            
         }
     }
 }
